f926f7b8fb9dd6cdc96b91f9976a9c235b06044c
Royal <royalhuseyn902@gmail.com>
2024-01-23 02:12:43 +0400

    Create func for getting data from wheather API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './style.css';
+
+async function getWheatherData(location) {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=6f555dcee5874f618c993013242001&q=${location}&days=4`,
+      { mode: 'cors' },
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.error.message);
+    }
+
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+getWheatherData('baku');
