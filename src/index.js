84d7d172c30e47a737883f24128d2036eccfc960
Royal <royalhuseyn902@gmail.com>
2024-01-23 17:08:15 +0400

    Create funcs to get only required data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,51 @@
 import './style.css';
 
+function getForecastData(day, data) {
+  return {
+    date: data.forecast.forecastday[day].date,
+    day: {
+      condition: {
+        text: data.forecast.forecastday[day].day.condition.text,
+        icon: data.forecast.forecastday[day].day.condition.icon,
+      },
+      daily_chance_of_rain:
+        data.forecast.forecastday[day].day.daily_chance_of_rain,
+      avgtemp_c: data.forecast.forecastday[day].day.avgtemp_c,
+      avgtemp_f: data.forecast.forecastday[day].day.avgtemp_f,
+      avghumidity: data.forecast.forecastday[day].day.avghumidity,
+    },
+  };
+}
+
+function getRequiredData(data) {
+  return {
+    location: {
+      name: data.location.name,
+      country: data.location.country,
+      localtime: data.location.localtime,
+    },
+    current: {
+      condition: {
+        text: data.current.condition.text,
+        icon: data.current.condition.icon,
+      },
+      temp_c: data.current.temp_c,
+      temp_f: data.current.temp_f,
+      feelslike_c: data.current.feelslike_c,
+      feelslike_f: data.current.feelslike_f,
+      humidity: data.current.humidity,
+    },
+    forecast: {
+      forecastday: [
+        getForecastData(0, data),
+        getForecastData(1, data),
+        getForecastData(2, data),
+        getForecastData(3, data),
+      ],
+    },
+  };
+}
+
 async function getWheatherData(location) {
   try {
     const response = await fetch(
@@ -13,7 +59,8 @@ async function getWheatherData(location) {
       throw new Error(data.error.message);
     }
 
-    console.log(data);
+    const requiredData = getRequiredData(data);
+    console.log(requiredData);
   } catch (error) {
     console.log(error.message);
   }
