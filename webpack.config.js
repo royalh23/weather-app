5f840ddfe5b0a7c870f2a2462543fb48272a13c7
Royal <royalhuseyn902@gmail.com>
2024-01-24 22:53:12 +0400

    Switch to production mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/template.html',
     }),
   ],
-  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
